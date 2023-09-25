56d34a219425e3ef1c9e629f8ac8c61053723965
RaviPratihast <ravipratihast@gmail.com>
2023-09-25 11:56:56 +0530

    refactor & modal window added

--- src/pages/dashboard-page/dashboard.jsx
+++ src/pages/dashboard-page/dashboard.jsx
@@ -227,6 +227,7 @@ import { useProfile } from "../../context/add-profile-context";
 
 export const Dashboard = () => {
   const { state } = useProfile();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => {
     setIsModalOpen(true);
@@ -235,7 +236,7 @@ export const Dashboard = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  // console.log(state);
+
   return (
     <div className="border md:h-screen w-screen max-sm:border-assignBlue max-sm:border-2 flex pl-20 pr-20 pt-10 sm:pb-10">
       <div className=" w-full h-full flex gap-20">
@@ -645,30 +646,89 @@ export const Dashboard = () => {
             </div>
             {/* add  */}
             <div className=" shadow-lg border-2  rounded-2xl border-[#e2e8f0] w-1/2 max-lg:w-full  flex items-center justify-center max-sm:mb-4">
-              <div className="flex flex-col items-center justify-center gap-3 max-sm:h-60">
-                <div className="h-16 w-16 rounded-full bg-[#F5F5F5] flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="52"
-                    height="52"
-                    viewBox="0 0 52 52"
-                    fill="none"
-                    onClick={openModal}
-                    className="cursor-pointer"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M26 10.0049C26.8835 10.0049 27.5997 10.7211 27.5997 11.6046L27.5997 24.7097L40.7048 24.7096C41.5883 24.7096 42.3045 25.4258 42.3045 26.3093C42.3045 27.1927 41.5883 27.9089 40.7048 27.9089L27.5998 27.909L27.5998 41.0139C27.5998 41.8974 26.8836 42.6136 26.0002 42.6136C25.1167 42.6136 24.4005 41.8974 24.4005 41.0139L24.4004 27.909L11.2953 27.9089C10.4119 27.9089 9.69567 27.1927 9.69568 26.3092C9.69568 25.4258 10.4119 24.7096 11.2953 24.7096L24.4004 24.7097L24.4003 11.6046C24.4003 10.7211 25.1165 10.0049 26 10.0049Z"
-                      fill="#999CA0"
-                    />
-                  </svg>
+              {state.length !== 0 ? (
+                state.map(({ Name, Contact, Email, Instagram, Youtube }) => {
+                  return (
+                    <div className="w-full h-full p-5">
+                      <h1 className="text-2xl font-bold">{Name}</h1>
+                      <div className="p-4">
+                        <div className="flex justify-between max-lg:flex-col ">
+                          {/* contact */}
+                          <div className="flex w-1/2 items-center justify-start gap-3 max-lg:w-full">
+                            <div className="bg-[#E9F9EB] w-8 h-8 rounded-full flex items-center justify-center">
+                              <img
+                                src="/assets/contact_logo.png"
+                                alt="contact"
+                              />
+                            </div>
+                            <h2 className="text-sm underline underline-offset-1">
+                              {Contact}
+                            </h2>
+                          </div>
+                          <div className="flex w-1/2 items-center justify-start gap-3 max-lg:w-full">
+                            <div className="bg-[#FFE9EC]  w-8 h-8 rounded-full flex items-center justify-center">
+                              <img
+                                src="/assets/logo-instagram.png"
+                                alt="instagram"
+                              />
+                            </div>
+                            <h2 className="text-sm underline underline-offset-1">
+                              {Instagram}
+                            </h2>
+                          </div>
+                        </div>
+                        <div className=" flex justify-between max-lg:flex-col mt-2">
+                          {/* email & youtube */}
+                          <div className="flex w-1/2 items-center justify-start gap-3 max-lg:w-full">
+                            <div className="bg-[#E4DFF2] w-8 h-8 rounded-full flex items-center justify-center">
+                              <img src="/assets/Mail.png" alt="mail" />
+                            </div>
+                            <h2 className="text-sm underline underline-offset-1">
+                              {Email}
+                            </h2>
+                          </div>
+                          <div className="flex w-1/2 items-center justify-start gap-3 max-lg:w-full">
+                            <div className="bg-[#FFE9E9] w-8 h-8 rounded-full flex items-center justify-center">
+                              <img
+                                src="/assets/logo-youtube.png"
+                                alt="youtube"
+                              />
+                            </div>
+                            <h2 className="text-sm underline underline-offset-1">
+                              {Youtube}
+                            </h2>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                <div className="flex flex-col items-center justify-center gap-3 max-sm:h-60">
+                  <div className="h-16 w-16 rounded-full bg-[#F5F5F5] flex items-center justify-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="52"
+                      height="52"
+                      viewBox="0 0 52 52"
+                      fill="none"
+                      onClick={openModal}
+                      className="cursor-pointer"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M26 10.0049C26.8835 10.0049 27.5997 10.7211 27.5997 11.6046L27.5997 24.7097L40.7048 24.7096C41.5883 24.7096 42.3045 25.4258 42.3045 26.3093C42.3045 27.1927 41.5883 27.9089 40.7048 27.9089L27.5998 27.909L27.5998 41.0139C27.5998 41.8974 26.8836 42.6136 26.0002 42.6136C25.1167 42.6136 24.4005 41.8974 24.4005 41.0139L24.4004 27.909L11.2953 27.9089C10.4119 27.9089 9.69567 27.1927 9.69568 26.3092C9.69568 25.4258 10.4119 24.7096 11.2953 24.7096L24.4004 24.7097L24.4003 11.6046C24.4003 10.7211 25.1165 10.0049 26 10.0049Z"
+                        fill="#999CA0"
+                      />
+                    </svg>
+                  </div>
+                  <h3 className="text-sm font-bold text-[#8C8C8C]">
+                    Add Profile
+                  </h3>
+                  <Modal isOpen={isModalOpen} onClose={closeModal} />
                 </div>
-                <h3 className="text-sm font-bold text-[#8C8C8C]">
-                  Add Profile
-                </h3>
-                <Modal isOpen={isModalOpen} onClose={closeModal} />
-              </div>
+              )}
             </div>
           </div>
         </div>
